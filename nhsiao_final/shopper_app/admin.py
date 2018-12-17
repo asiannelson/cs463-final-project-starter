85ed0b68e11c882a277955fff30a5926965f1ca8
Nelson <nelson14h@yahoo.com>
2018-12-17 15:12:25 -0700

    nhsiao_final removed

--- nhsiao_final/shopper_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Item, GeoLoc, ItemLocation
-
-admin.site.register(Item)
-admin.site.register(GeoLoc)
-admin.site.register(ItemLocation)
--- nhsiao_final/shopper_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopperAppConfig(AppConfig):
-    name = 'shopper_app'
